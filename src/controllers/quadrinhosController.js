7e6ea863a587a14263146e60e4333fada46203e4
julia <juhcdgomes@gmail.com>
2025-09-22 09:54:17 -0300

    adiciona criador de quadrinhos

--- src/controllers/quadrinhosController.js
+++ src/controllers/quadrinhosController.js
@@ -13,7 +13,7 @@ const getAllQuadrinhos = (req, res) => {
 
 const getQuadrinhosById = (req, res) => {
      const id = parseInt(req.params.id);
-  const quadrinho = quadrinhos.find(p => p.id === id);
+  const quadrinho = quadrinhos.find(q => q.id === id);
 
   if (quadrinho) {
       res.status(200).json(quadrinho);
@@ -24,4 +24,51 @@ const getQuadrinhosById = (req, res) => {
   }
 }
 
-export { getAllQuadrinhos, getQuadrinhosById };
+const getQuadrinhosByEditora = (req, res) => {
+    let editora = req.params.editora;
+    editora = editora.toLowerCase();
+
+  const quadrinhosFiltrados = quadrinhos.filter(q => q.editora.toLowerCase().includes(editora));
+
+  if (quadrinhosFiltrados) {
+      res.status(200).json(quadrinhosFiltrados);
+  } else {
+      res.status(404).json({
+          mensagem: "Não foi encontrado nenhum quadrinho dessa editora."
+      })
+  }
+}
+
+const createQuadrinho = (req, res) => {
+    const { titulo, editora, numero, saga, heroi, autor, anoPublicacao, preco } = req.body;
+
+    if (preco <= 0) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'preço' deve ser maior que 0."
+        });
+    }
+
+ const novoQuadrinho = {
+        id: quadrinhos.length + 1,
+        titulo,
+        editora,
+        numero,
+        saga,
+        heroi,
+        autor,
+        anoPublicacao,
+        preco
+    }
+
+    quadrinhos.push(novoQuadrinho);
+    res.status(201).json({
+        success: true,
+        message: "Quadrinho cadastrado com sucesso",
+        quadrinho: novoQuadrinho
+    })
+}
+
+
+
+export { getAllQuadrinhos, getQuadrinhosById, getQuadrinhosByEditora, createQuadrinho, deleteQuadrinho };
--- src/routes/quadrinhosRoutes.js
+++ src/routes/quadrinhosRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllQuadrinhos, getQuadrinhosById } from "../controllers/quadrinhosController.js";
+import { getAllQuadrinhos, getQuadrinhosByEditora, getQuadrinhosById } from "../controllers/quadrinhosController.js";
 
 const router = express.Router();
 
 router.get("/", getAllQuadrinhos);
 router.get("/:id", getQuadrinhosById);
+router.get("/editora/:editora", getQuadrinhosByEditora);
 
 export default router;
